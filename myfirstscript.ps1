d83e749146b913ef8a38e26738853558de236433
yorin <werbyvillarino@gmail.com>
2015-03-26 11:05:22 +0800

    3/26/2015

--- myfirstscript.ps1
+++ myfirstscript.ps1
@@ -1,2 +1,12 @@
-Write-Host "Hello, World!"
+Write-Host "Hello, World!" -backgroundcolor "red"
 [console]::beep(2000,500)
+
+Write-Host `
+    "This is a continuation of the line."
+$a = 1
+$b = 2
+$c = $a + $b
+$c
+
+$strA = “fast”
+Write-Host “Tom drives a $strA car.”
